577ae1686b87d88b02bbabe0cecb9dfb35575f0c
Josh Theriault <joshriaul@gmail.com>
2024-02-19 21:51:47 -0400

    Make improvements to the new-problem.bat and new-problem-java.bat scripts

--- new-problem-java.bat
+++ new-problem-java.bat
@@ -4,27 +4,33 @@
 if not [%1]==[] set problemname=%1
 if [%1]==[] set /p problemname="Name of problem: "
 
-mkdir "problems/%problemname%"
+if not exist "problems/%problemname%" mkdir "problems/%problemname%"
 
 :: make link to website
-echo start https://open.kattis.com/problems/%problemname% > problems/%problemname%/kattis.bat
+if not exist "problems/%problemname%/kattis.bat" echo start https://open.kattis.com/problems/%problemname%> problems/%problemname%/kattis.bat
 
 :: make base file
-echo. > problems/%problemname%/%problemname%.java
+if not exist "problems/%problemname%/%problemname%.java" echo:> problems/%problemname%/%problemname%.java
 
 :: make build file
-echo javac %problemname%.java -encoding UTF-8 > problems/%problemname%/build.bat
-echo pause >> problems/%problemname%/build.bat
+if not exist "problems/%problemname%/build.bat" (
+	echo javac %problemname%.java -encoding UTF-8> problems/%problemname%/build.bat
+	echo pause>> problems/%problemname%/build.bat
+)
 
 :: make run file
-echo java %problemname% >> problems/%problemname%/run.bat
-echo pause >> problems/%problemname%/run.bat
+if not exist "problems/%problemname%/run.bat" (
+	echo java %problemname%>> problems/%problemname%/run.bat
+	echo pause>> problems/%problemname%/run.bat
+)
 
 :: make run with input file
-echo java %problemname% ^< input.txt >> problems/%problemname%/run-with-input.bat
-echo pause >> problems/%problemname%/run-with-input.bat
+if not exist "problems/%problemname%/run-with-input.bat" (
+	echo java %problemname% ^< input.txt>> problems/%problemname%/run-with-input.bat
+	echo pause>> problems/%problemname%/run-with-input.bat
+)
 
 :: make input file
-echo. > problems/%problemname%/input.txt
+if not exist "problems/%problemname%/input.txt" echo:> problems/%problemname%/input.txt
 
 pause
--- new-problem.bat
+++ new-problem.bat
@@ -4,27 +4,41 @@
 if not [%1]==[] set problemname=%1
 if [%1]==[] set /p problemname="Name of problem: "
 
-mkdir "problems/%problemname%"
+if not exist "problems/%problemname%" mkdir "problems/%problemname%"
 
 :: make link to website
-echo start https://open.kattis.com/problems/%problemname% > problems/%problemname%/kattis.bat
+if not exist "problems/%problemname%/kattis.bat" echo start https://open.kattis.com/problems/%problemname%> problems/%problemname%/kattis.bat
 
 :: make base file
-echo. > problems/%problemname%/main.cpp
+if not exist "problems/%problemname%/main.cpp" (
+	echo #include ^<iostream^>>> problems/%problemname%/main.cpp
+	echo:>> problems/%problemname%/main.cpp
+	echo int main^(^)>> problems/%problemname%/main.cpp
+	echo {>> problems/%problemname%/main.cpp
+	echo.	std::ios::sync_with_stdio^(false^)^;>> problems/%problemname%/main.cpp
+	echo.	std::cin.tie^(nullptr^)^;>> problems/%problemname%/main.cpp
+	echo }>> problems/%problemname%/main.cpp
+)
 
 :: make build file
-echo g++ -g -O2 -std=gnu++17 -static main.cpp -o %problemname% > problems/%problemname%/build.bat
-echo pause >> problems/%problemname%/build.bat
+if not exist "problems/%problemname%/build.bat" (
+	echo g++ -g -O2 -std=gnu++17 -static main.cpp -o %problemname%> problems/%problemname%/build.bat
+	echo pause>> problems/%problemname%/build.bat
+)
 
 :: make run file
-echo %problemname%.exe >> problems/%problemname%/run.bat
-echo pause >> problems/%problemname%/run.bat
+if not exist "problems/%problemname%/run.bat" (
+	echo %problemname%.exe>> problems/%problemname%/run.bat
+	echo pause>> problems/%problemname%/run.bat
+)
 
 :: make run with input file
-echo %problemname%.exe ^< input.txt >> problems/%problemname%/run-with-input.bat
-echo pause >> problems/%problemname%/run-with-input.bat
+if not exist "problems/%problemname%/run-with-input.bat" (
+	echo %problemname%.exe ^< input.txt>> problems/%problemname%/run-with-input.bat
+	echo pause>> problems/%problemname%/run-with-input.bat
+)
 
 :: make input file
-echo. > problems/%problemname%/input.txt
+if not exist "problems/%problemname%/input.txt" echo:> problems/%problemname%/input.txt
 
 pause
